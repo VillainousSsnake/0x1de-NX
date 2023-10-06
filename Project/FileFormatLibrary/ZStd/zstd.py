77dccd25598d5b411c31d7376b89d5e4c80911fb
VillainousSsnake <144048072+VillainousSsnake@users.noreply.github.com>
2023-10-06 16:17:16 -0400

    ZStandard Library V0.1

---
+++ Project/FileFormatLibrary/ZStd/zstd.py
@@ -0,0 +1,55 @@
+"""
+This is the class for the ZStandard files (.zs)
+"""
+
+import zstandard as zstd
+
+
+class ZSTD:
+
+    @staticmethod
+    def decompress(input_file, output_file, dictionary):
+
+        # Load the dictionary
+        with open(dictionary, 'rb') as f:
+            dict_data = f.read()
+        zstd_dict = zstd.ZstdCompressionDict(dict_data)
+
+        # Create a decompression context using the dictionary
+        dctx = zstd.ZstdDecompressor(dict_data=zstd_dict)
+
+        # Decompress the file
+        with open(input_file, 'rb') as f_in, open(output_file, 'wb') as f_out:
+            f_out.write(dctx.decompress(f_in.read()))
+    @staticmethod
+    def compress(input_file, output_file, dictionary):
+        # Load the dictionary
+        with open(dictionary, "rb") as f:
+            dict_data = f.read()
+        zstd_dict = zstd.ZstdCompressionDict(dict_data)
+
+        # Create a level 16 compression context using the dictionary
+        dctx = zstd.ZstdCompressor(level=16, dict_data=zstd_dict)
+
+        # Compressing the file
+        with open(input_file, "rb") as f_in, open(output_file, "wb") as f_out:
+            f_out.write(dctx.compress(f_in.read()))
+
+        print("Compressed File")
+
+    @staticmethod
+    def read(input_file, dictionary):
+        # Load the dictionary
+        with open(dictionary, 'rb') as f:
+            dict_data = f.read()
+        zstd_dict = zstd.ZstdCompressionDict(dict_data)
+
+        # Create a decompression context using the dictionary
+        dctx = zstd.ZstdDecompressor(dict_data=zstd_dict)
+
+        # Decompress the file
+        with open(input_file, 'rb') as f_in:
+            output = dctx.decompress(f_in.read())
+
+        # returning the output
+        return output
--- Project/main.py
+++ Project/main.py
@@ -4,8 +4,10 @@
 """
 
 from tkinter import filedialog
+from tkinter import *
 import turtle
 from __Cache__.data import *
+from FileFormatLibrary.ZStd.zstd import ZSTD
 
 
 # program
@@ -15,3 +17,16 @@ class Program:
 
         window = turtle.Screen()
         window.title("0x1de NX - VERSION: Dev1.0.0")
+        turtleCanvas = window.getcanvas()
+        root = turtleCanvas._rootwindow
+        root.resizable(False, False)
+        def on_close():
+            turtle.bye()
+        root.protocol("WM_DELETE_WINDOW", on_close)
+
+        # the window mainloop
+        window.mainloop()
+
+
+# running the program
+Program.main()
