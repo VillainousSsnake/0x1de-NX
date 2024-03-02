ce1a71ca477f687d08362209195a5858f4614d63
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-02 12:48:58 -0500

    Added standard HigharcheySDL code to main_menu function

--- App/GUI/main_menu.py
+++ App/GUI/main_menu.py
@@ -4,9 +4,29 @@
 
 # Importing modules and libraries:
 import customtkinter as ctk
+from tkinter import messagebox, filedialog
 
 
 # main_menu function
 def main_menu(app):
 
-    pass  # TODO: Stub
+    # Setting theme
+    ctk.set_appearance_mode(app.settings["current_theme"])
+
+    # Creating root window
+    root = ctk.CTk()
+    root.title("AINB-Toolbox - VillainousSsnake - " + Updater.get_current_version())
+    root.geometry("850x525+200+200")
+
+    # Defining on_close function
+    def on_close():
+        root.destroy()
+        app.returnStatement = "exit"
+
+    # Assigning the buttons on the tkinter window top bar
+    root.protocol("WM_DELETE_WINDOW", on_close)
+
+    # TODO: Code goes here
+
+    # Root mainloop
+    root.mainloop()
