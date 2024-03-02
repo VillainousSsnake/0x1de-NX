2b19230caf0a233472e8bcacd24243343fcf534e
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-02 12:32:59 -0500

    Added main_menu method

--- App/AppLib/index.py
+++ App/AppLib/index.py
@@ -2,9 +2,13 @@
 # Contains GUI index
 
 # Importing GUI
-from App.GUI import *
+from App.GUI.main_menu import main_menu as main_menu_redirect
 
 
 # Index class (Holds menu functions)
 class Index:
-    pass  # Stub
+
+    @staticmethod
+    def main_menu(app):
+
+        main_menu_redirect(app)
