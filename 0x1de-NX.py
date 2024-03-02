81efe8e27eb889f7c27ca7b5f93c463c10fa1ac7
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-02 12:35:53 -0500

    Added comments

--- 0x1de-NX.py
+++ 0x1de-NX.py
@@ -1,9 +1,11 @@
 # /0x1de-NX.py
 # Holds main program code
 
+# Importing modules
 from App.AppLib.app import App
 from App.AppLib.index import Index
 
+# Creating App variable
 app = App()
 
 # Mainloop
