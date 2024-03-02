2c8f72d7d4383033498dd4fca2af6bab848541bc
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-02 12:22:57 -0500

    Added main_menu.py

---
+++ App/GUI/main_menu.py
@@ -0,0 +1,11 @@
+# /App/GUI/main_menu.py
+# Contains main menu code
+
+
+# Importing modules and libraries:
+import customtkinter as ctk
+
+
+# main_menu function
+def main_menu():
+    pass  # TODO: Stub
