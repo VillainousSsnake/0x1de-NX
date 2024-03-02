21696354d5cfbdbf27cde7f3607a00c9dde64f05
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-02 14:15:51 -0500

    Added updater.py, Added Updater class

---
+++ App/AppLib/updater.py
@@ -0,0 +1,13 @@
+# /App/AppLib/updater.py
+# Contains Updater class
+
+# Global Variables
+CurrentVersionGlobal = "Alpha 0.0.1"
+
+
+# Updater class
+class Updater:
+    @staticmethod
+    def get_current_version():
+
+        return CurrentVersionGlobal
