6579627e9f7f0568744830b85b6b2f7c7ffb59af
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-03 15:11:11 -0500

    Added import statements

--- App/GUI/SubWin/main_menu/new_project.py
+++ App/GUI/SubWin/main_menu/new_project.py
@@ -1,4 +1,10 @@
 # /App/GUI/SubWin/new_project.py
 
+# Importing modules and/or libraries
+from tkinter import filedialog
+import customtkinter as ctk
+import os
+
+
 def new_project(root, app):
     pass  # TODO: Stub
