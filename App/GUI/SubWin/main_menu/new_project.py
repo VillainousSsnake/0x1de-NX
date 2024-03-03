3876888d9352a9ad61eae34c65b7000727ad4c8f
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-03 15:34:48 -0500

    Added creation of toplevel

--- App/GUI/SubWin/main_menu/new_project.py
+++ App/GUI/SubWin/main_menu/new_project.py
@@ -1,10 +1,17 @@
 # /App/GUI/SubWin/new_project.py
 
 # Importing modules and/or libraries
+from App.AppLib.updater import Updater
 from tkinter import filedialog
 import customtkinter as ctk
 import os
 
 
 def new_project(root, app):
-    pass  # TODO: Stub
+
+    # Creating the window
+    window = ctk.CTkToplevel()
+    window.title("0x1de NX | " + Updater.get_current_version() + " | New Project")
+    window.geometry("600x400")
+    window.attributes("-topmost", True)
+    window.resizable(False, False)
