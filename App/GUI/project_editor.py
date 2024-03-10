7e981509897dfb972eacc874c8955bf7c69b6f1d
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-10 13:08:13 -0400

    Added some comments and import statements

--- App/GUI/project_editor.py
+++ App/GUI/project_editor.py
@@ -1,4 +1,10 @@
 # /App/GUI/project_editor.py
+# Contains code for project editor
 
+# Importing libraries and modules
+import customtkinter as ctk
+
+
+# Defining project_editor
 def project_editor(app):
     pass  # TODO: Stub
