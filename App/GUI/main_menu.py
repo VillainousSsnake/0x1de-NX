a31fe8d34aebcb81bba64c076474bd90d9b8fc6e
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-09 20:25:39 -0500

    Added settings menu

--- App/GUI/main_menu.py
+++ App/GUI/main_menu.py
@@ -18,9 +18,72 @@ from PIL import Image, ImageTk
 import os
 
 
+# ButtonFunc class (Adapted from AINB-Toolbox)
+# Button func
+class ButtonFunc:
+    @staticmethod
+    def ainb_to_code_option_menu_button_command(app, event=None):
+
+        print(event)
+
+        match event:
+
+            case "JSON":
+
+                app.settings['ainb_code_format'] = 'JSON'
+                Config.overwrite_setting('ainb_code_format', event)
+
+            case "YAML":
+
+                app.settings['ainb_code_format'] = 'YAML'
+                Config.overwrite_setting('ainb_code_format', event)
+
+    @staticmethod
+    def romfs_path_browse_button_command(app, romfs_path_entry):
+        romfs_path = filedialog.askdirectory(title="Select Tears of the Kingdom RomFS Folder")
+        if romfs_path == "":
+            return 0
+        else:
+            app.settings["romfs_path"] = romfs_path
+            Config.overwrite_setting("romfs_path", romfs_path)
+            romfs_path_entry.delete(0, "end")
+            romfs_path_entry.insert(0, romfs_path)
+
+
 # _func class (Contains functions that the menu function uses)
 # noinspection PyPep8Naming, PyUnusedLocal
 class _func:
+    @staticmethod
+    def focus_in_romfs_entry(romfs_path_label, event=None):
+        romfs_path_label.configure(
+            text="Game Dump Location*"
+        )
+
+    @staticmethod
+    def update_romfs_entry(app, romfs_path_entry, romfs_path_label, event=None):
+        romfs_path = romfs_path_entry.get()
+        app.settings["romfs_path"] = romfs_path
+        Config.overwrite_setting("romfs_path", romfs_path)
+        romfs_path_label.configure(
+            text="Game Dump Location"
+        )
+
+    @staticmethod
+    def font_size_entry_return_command(app, font_size_label, font_size_entry, event=None):
+        Text = "Font Size"
+        font_size_label.configure(
+            text=Text
+        )
+        app.settings["font_size"] = font_size_entry.get()
+        Config.overwrite_setting("font_size", font_size_entry.get())
+
+    @staticmethod
+    def font_size_entry_keys_command(app, font_size_label, event=None):
+        Text = "Font Size*"
+        font_size_label.configure(
+            text=Text
+        )
+
     @staticmethod
     def update_search_bar_projects_menu(self, segmented_button_controller, event=None):
         # Updating segmented_button_menu_controller
@@ -307,8 +370,6 @@ class SegmentedButtonMenu:
 
             self.variables["Projects"] = new_projects_list
 
-
-
         # Creating the navigation frame
         nav_frame = ctk.CTkFrame(
             master=self.master,
@@ -545,8 +606,71 @@ class SegmentedButtonMenu:
             )
             nothing_here_label.pack(fill='both')
 
-    def create_settings_menu(self):
-        pass  # TODO: Stub
+    def create_settings_menu(self, app):
+
+        scroll_frame = ctk.CTkScrollableFrame(master=self.master, width=1000, fg_color="#242424")
+        scroll_frame.pack(fill='both', side='left')
+        self.object_list.append(scroll_frame)
+
+        romfs_path_label = ctk.CTkLabel(
+            master=scroll_frame,
+            text="Game Dump Location", anchor='w', width=135,
+            corner_radius=5, fg_color="#3B8ED0"
+        )
+        romfs_path_label.grid(row=0, column=0, padx=20, pady=10)
+
+        romfs_path_entry = ctk.CTkEntry(master=scroll_frame)
+        if app.settings["romfs_path"] is None:
+            romfs_path_entry.configure(placeholder_text="Eg. (D:\\Tears of the Kingdom\\romfs)")
+        else:
+            romfs_path_entry.insert(0, app.settings["romfs_path"])
+        romfs_path_entry.grid(row=0, column=1, padx=20, pady=10)
+        romfs_path_entry_command_partial = partial(_func.update_romfs_entry, app, romfs_path_entry, romfs_path_label)
+        romfs_path_entry_focus_partial = partial(_func.focus_in_romfs_entry, romfs_path_label)
+        romfs_path_entry.bind("<Return>", romfs_path_entry_command_partial)
+        romfs_path_entry.bind("<Key>", romfs_path_entry_focus_partial)
+
+        romfs_browse_command = partial(ButtonFunc.romfs_path_browse_button_command, app, romfs_path_entry)
+        romfs_path_browse = ctk.CTkButton(
+            scroll_frame,
+            text="Browse...", fg_color="grey",
+            command=romfs_browse_command,
+        )
+        romfs_path_browse.grid(row=0, column=3)
+
+        # AINB-To-Code Setting
+        ainb_to_code_format_label = ctk.CTkLabel(
+            master=scroll_frame,
+            text="AINB-To-Code Format", anchor='w', width=135,
+            corner_radius=5, fg_color="#3B8ED0"
+        )
+        ainb_to_code_format_label.grid(row=2, column=0, padx=20, pady=10)
+
+        ainb_to_code_option_menu_command = partial(ButtonFunc.ainb_to_code_option_menu_button_command, app)
+        ainb_to_code_format_option_menu = ctk.CTkOptionMenu(
+            master=scroll_frame,
+            values=["YAML", "JSON"],
+            command=ainb_to_code_option_menu_command,
+        )
+        ainb_to_code_format_option_menu.set(app.settings["ainb_code_format"])
+        ainb_to_code_format_option_menu.grid(row=2, column=1, padx=20, pady=10)
+
+        # Font size setting
+        font_size_label = ctk.CTkLabel(
+            master=scroll_frame,
+            text="Font Size", anchor='w', width=135,
+            corner_radius=5, fg_color="#3B8ED0"
+        )
+        font_size_label.grid(row=3, column=0, padx=20, pady=10)
+
+        font_size_entry = ctk.CTkEntry(master=scroll_frame)
+        font_size_entry.insert("0", app.settings["font_size"])
+        font_size_entry.grid(row=3, column=1, padx=20, pady=10)
+        font_size_entry_keys_command = partial(_func.font_size_entry_keys_command, app, font_size_label)
+        font_size_entry_return_command = partial(_func.font_size_entry_return_command, app, font_size_label,
+                                                 font_size_entry)
+        font_size_entry.bind("<Key>", font_size_entry_keys_command)
+        font_size_entry.bind("<Return>", font_size_entry_return_command)
 
     def create_community_menu(self):
         pass  # TODO: Stub
@@ -560,7 +684,7 @@ class SegmentedButtonMenu:
         self.object_list[3].pack(fill='both', side='left')
 
     def show_settings_menu(self):
-        pass  # TODO: Stub
+        self.object_list[4].pack(fill='both', side='left')
 
     def show_community_menu(self):
         pass  # TODO: Stub
@@ -776,7 +900,7 @@ def main_menu(app):
     # Creating all the menus
     segmented_button_controller.create_projects_menu(segmented_button_controller)
     segmented_button_controller.create_plugins_menu()
-    segmented_button_controller.create_settings_menu()
+    segmented_button_controller.create_settings_menu(app)
     segmented_button_controller.create_community_menu()
 
     # Showing the project menu (Since it's the default)
