65b785840cb1b526b0c9f86ab39a0a2389233004
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-20 12:50:03 -0400

    FIxed some spacing

--- App/AppLib/texture_handler.py
+++ App/AppLib/texture_handler.py
@@ -10,11 +10,14 @@ class TextureHandle:
 
     @staticmethod
     def get_texture_directory() -> str:
+
         # TODO: Replace os.getcwd() with os.getenv('LOCALAPPDATA')
         texture_folder_path = os.path.join(
             os.getcwd(), "App", "Image", "Tex"
         )
+
         # Creating the directories if they don't exist
         if not os.path.exists(texture_folder_path):
             os.makedirs(texture_folder_path)
+
         return texture_folder_path
