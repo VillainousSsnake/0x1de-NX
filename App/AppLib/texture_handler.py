79c33cb451f403b8f98655f1e67332a6cab820ce
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-20 12:48:05 -0400

    Added texture_handler.py to AppLib

---
+++ App/AppLib/texture_handler.py
@@ -0,0 +1,20 @@
+# /App/AppLib/texture_handler.py
+# This is a texture handler module
+
+# Importing modules and libraries
+import os
+
+
+# The texture handler class
+class TextureHandle:
+
+    @staticmethod
+    def get_texture_directory() -> str:
+        # TODO: Replace os.getcwd() with os.getenv('LOCALAPPDATA')
+        texture_folder_path = os.path.join(
+            os.getcwd(), "App", "Image", "Tex"
+        )
+        # Creating the directories if they don't exist
+        if not os.path.exists(texture_folder_path):
+            os.makedirs(texture_folder_path)
+        return texture_folder_path
