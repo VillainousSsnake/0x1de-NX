eb5983e3c920e14da2e7b2924ef88efabac44244
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-23 13:42:15 -0400

    Fixed plugin refreshing

--- App/AppLib/plugin_handler.py
+++ App/AppLib/plugin_handler.py
@@ -10,37 +10,34 @@ import os
 class PluginHandler:
     @staticmethod
     def get_plugin_folder() -> os.path:
+
         # TODO: Replace os.getcwd() with os.getenv('LOCALAPPDATA')
         plugins_folder = os.path.join(
             os.getcwd(), "0x1de-NX", "Plugins",
         )
+
         # Creating the directories if they don't exist
         if not os.path.exists(plugins_folder):
             os.makedirs(plugins_folder)
+
+        # Refreshing the plugins folder
+
+        json_contents = {}
+
+        for item in os.listdir(plugins_folder):
+            if item != 'plugins.json':
+                json_contents[item] = False
+
+        with open(os.path.join(plugins_folder, 'plugins.json'), 'w') as f_out:
+            json.dump(json_contents, f_out)
+
+        # Returning the plugins folder path
         return plugins_folder
 
     @staticmethod
     def get_plugins() -> dict:
 
         plugins_folder = PluginHandler.get_plugin_folder()
-
-        # Creating the folder list
-        folder_list = os.listdir(plugins_folder)
-
-        # Creating the plugin config file if it doesn't exist
-        if (
-            not os.path.exists(os.path.join(plugins_folder, 'plugins.json'))
-            or json.load(open(os.path.join(plugins_folder, 'plugins.json'), 'r')) == {}
-        ):
-
-            json_contents = {}
-
-            for item in folder_list:
-                if item != 'plugins.json':
-                    json_contents[item] = False
-
-            with open(os.path.join(plugins_folder, 'plugins.json'), 'w') as f_out:
-                json.dump(json_contents, f_out)
 
         # Returning the contents
         with open(os.path.join(plugins_folder, 'plugins.json'), 'r') as f_in:
