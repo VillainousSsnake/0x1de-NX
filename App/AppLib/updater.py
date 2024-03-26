002f7f8d45f40d7caa28a100b77a7b874c71f197
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-26 11:42:52 -0400

    Added get_latest_version method

--- App/AppLib/updater.py
+++ App/AppLib/updater.py
@@ -1,6 +1,11 @@
 # /App/AppLib/updater.py
 # Contains Updater class
 
+# Importing libraries and modules
+import requests
+import json
+
+
 # Global Variables
 CurrentVersionGlobal = "Alpha 0.0.1"
 
@@ -11,3 +16,11 @@ class Updater:
     def get_app_version():
 
         return CurrentVersionGlobal
+
+    @staticmethod
+    def get_latest_version():
+
+        url = f"https://api.github.com/repos/VillainousSsnake/0x1de-NX/tags"
+        response = requests.get(url)
+        data = json.loads(response.text)
+        return data[0]['name'] if data else None
