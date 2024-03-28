20b72a1b081853fd9480e23bcb8580a6409e0a89
VillainousSsnake <cjdiamond9@gmail.com>
2024-03-28 15:13:31 -0400

    Added file_handler.py file

---
+++ App/AppLib/file_handler.py
@@ -0,0 +1,12 @@
+# /App/AppLib/texture_handler.py
+# This is a file handler module
+
+# Importing modules and libraries
+import os
+
+
+# The file handler class
+class FileHandler:
+
+    def __init__(self):
+        pass    # TODO: Stub
