9966e9fc2d9ddbeab9b96213660f3644e5a1aa77
VillainousSsnake <cjdiamond9@gmail.com>
2024-04-08 17:30:40 -0400

    Moved clearing temp code

--- 0x1de-NX.py
+++ 0x1de-NX.py
@@ -10,13 +10,6 @@ import os
 from App.AppLib.app import App
 from App.AppLib.index import Index
 
-
-# Clearing the temp folder
-temp_folder = os.path.join(os.getenv("LOCALAPPDATA"), "0x1de-NX", "_temp_")
-if os.path.exists(temp_folder):
-    shutil.rmtree(temp_folder)
-
-
 # Removing the splash screen
 if getattr(sys, 'frozen', False):
     import pyi_splash
@@ -38,3 +31,8 @@ while app.returnStatement != "exit":
 
         case "project_editor":  # Project Editor Menu
             Index.project_editor(app)
+
+# Clearing the temp folder
+temp_folder = os.path.join(os.getenv("LOCALAPPDATA"), "0x1de-NX", "_temp_")
+if os.path.exists(temp_folder):
+    shutil.rmtree(temp_folder)
