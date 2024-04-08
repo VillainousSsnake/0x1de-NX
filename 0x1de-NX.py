71167e62f3f705edac4e8037dc8ba026f834933f
VillainousSsnake <cjdiamond9@gmail.com>
2024-04-08 18:16:05 -0400

    Added code to delete temp at start, moved remove splashscreen code to after app was initialized

--- 0x1de-NX.py
+++ 0x1de-NX.py
@@ -10,14 +10,18 @@ import os
 from App.AppLib.app import App
 from App.AppLib.index import Index
 
+# Clearing the temp folder
+temp_folder = os.path.join(os.getenv("LOCALAPPDATA"), "0x1de-NX", "_temp_")
+if os.path.exists(temp_folder):
+    shutil.rmtree(temp_folder)
+
+# Creating App variable
+app = App()
+
 # Removing the splash screen
 if getattr(sys, 'frozen', False):
     import pyi_splash
     pyi_splash.close()
-
-
-# Creating App variable
-app = App()
 
 
 # Mainloop
