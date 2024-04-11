58492947b77585fd803d42780d9c912fee7c02e5
VillainousSsnake <cjdiamond9@gmail.com>
2024-04-11 19:51:30 -0400

    Moved romfs path detection to 0x1de-NX.py

--- App/GUI/main_menu.py
+++ App/GUI/main_menu.py
@@ -131,39 +131,6 @@ class _func:
         info_label2.configure(fg_color=fgc)
         button.configure(fg_color=fgc)
 
-    @staticmethod
-    def verify_romfs_path(app):
-
-        # Detecting if the romfs_path is None
-        if app.settings["romfs_path"] is None:
-
-            # Asking user to provide romfs path
-            continue_prompt = False
-
-            # While continue prompt is no loop
-            while continue_prompt is False:
-
-                messagebox.showinfo(
-                    "0x1de-NX Pop-up",
-                    "Please select your Zelda: Tears of the Kingdom RomFS dump folder.",
-                )
-                romfs_folder = filedialog.askdirectory(title="Select RomFS Folder Path")
-
-                if romfs_folder == "":
-
-                    message = """Do you want to continue without a romfs dump?
-        This will most likely cause a lot of errors in the future."""
-
-                    continue_prompt = messagebox.askyesno(
-                        "0x1de-NX Pop-up", message
-                    )
-
-                else:
-
-                    app.settings["romfs_path"] = romfs_folder
-                    Config.overwrite_setting("romfs_path", romfs_folder)
-                    continue_prompt = True
-
 
 # SegmentedButtonMenu class
 class SegmentedButtonMenu:
@@ -826,9 +793,6 @@ class ButtonCommand:
 
 # main_menu function
 def main_menu(app):
-
-    # Verifying if the romfs path is real
-    _func.verify_romfs_path(app)
 
     # Setting theme
     ctk.set_appearance_mode(app.settings["current_theme"])
