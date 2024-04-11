2c5ae7e73496a23433d354120789002dc4eb6566
VillainousSsnake <cjdiamond9@gmail.com>
2024-04-10 20:31:40 -0400

    Started working on wrapper for DT12345's BYML package

--- App/FFLib/BinaryYAML/__init__.py
+++ App/FFLib/BinaryYAML/__init__.py
@@ -1,4 +1,14 @@
+from App.FFLib.BinaryYAML.byml_base import Byml
+import os
+
 
 class BYML:
-    def __init__(self):
-        pass    # TODO: Stub
+    def __init__(self, file_path: os.PathLike | str):   # TODO: Finish
+
+        if not os.path.exists(file_path):
+            raise FileExistsError("File path given doesn't point to a real file! File path: " + str(file_path))
+
+        with open(file_path, "rb") as f_in:
+            raw_data = f_in.read()
+
+        self.byml_controller = Byml(raw_data)
