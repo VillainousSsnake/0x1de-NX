ead3712851e7efca9b7af5b29083f32468b6fac7
VillainousSsnake <cjdiamond9@gmail.com>
2024-04-11 16:05:57 -0400

    Finished __init__ method for BYML class

--- App/FFLib/BinaryYAML/__init__.py
+++ App/FFLib/BinaryYAML/__init__.py
@@ -3,12 +3,17 @@ import os
 
 
 class BYML:
-    def __init__(self, file_path: os.PathLike | str):   # TODO: Finish
+    def __init__(self, file_path: os.PathLike | str) -> None:
+        """
+        :param file_path: The input file path to the Binary YAML file
+        """
 
+        # Detecting if the path is real
         if not os.path.exists(file_path):
             raise FileExistsError("File path given doesn't point to a real file! File path: " + str(file_path))
 
+        # Getting the file data
         with open(file_path, "rb") as f_in:
             raw_data = f_in.read()
 
-        self.byml_controller = Byml(raw_data)
+        self.byml_controller = Byml(raw_data, os.path.basename(file_path))
