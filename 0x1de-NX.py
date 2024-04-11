106411240f5ff3a95725b2f125e4ce1a6b12f3bb
VillainousSsnake <cjdiamond9@gmail.com>
2024-04-11 19:51:20 -0400

    Moved romfs path detection to 0x1de-NX.py

--- 0x1de-NX.py
+++ 0x1de-NX.py
@@ -23,6 +23,34 @@ if getattr(sys, 'frozen', False):
     import pyi_splash
     pyi_splash.close()
 
+# Detecting if the romfs_path is None
+if app.settings["romfs_path"] is None:
+
+    # Asking user to provide romfs path
+    continue_prompt = False
+
+    # While continue prompt is no, loop
+    while continue_prompt is False:
+
+        messagebox.showinfo(
+            "0x1de-NX Pop-up",
+            "Please select your Zelda: Tears of the Kingdom RomFS dump folder.",
+        )
+        romfs_folder = filedialog.askdirectory(title="Select RomFS Folder Path")
+
+        if romfs_folder == "":
+
+            message = """Do you want to continue without a romfs dump?
+This will most likely cause a lot of errors in the future."""
+            continue_prompt = messagebox.askyesno(
+                "0x1de-NX Pop-up", message
+            )
+
+        else:
+            app.settings["romfs_path"] = romfs_folder
+            Config.overwrite_setting("romfs_path", romfs_folder)
+            continue_prompt = True
+
 
 # Mainloop
 while app.returnStatement != "exit":
