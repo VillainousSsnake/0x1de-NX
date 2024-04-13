3ffc8fd182a0b71446b9d33d4886ca43c523833d
VillainousSsnake <cjdiamond9@gmail.com>
2024-04-13 12:15:10 -0400

    Added new_dialog.py

---
+++ App/GUI/SubWin/project_editor/new_dialog.py
@@ -0,0 +1,10 @@
+# /App/GUI/SubWin/new_project.py
+# Contains code for new dialog
+
+# Importing modules, packages and libraries
+import App.AppLib.customtkinter as ctk
+
+
+# Defining new_dialog function
+def new_dialog():
+    pass    # TODO: Stub
