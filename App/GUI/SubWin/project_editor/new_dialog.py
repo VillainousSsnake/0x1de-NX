a8707fd548e06a5b68cbb05fc2cb03d0f15c6631
VillainousSsnake <cjdiamond9@gmail.com>
2024-04-13 12:59:47 -0400

    Added layout (Needs functionality)

--- App/GUI/SubWin/project_editor/new_dialog.py
+++ App/GUI/SubWin/project_editor/new_dialog.py
@@ -5,6 +5,75 @@
 import App.AppLib.customtkinter as ctk
 
 
+# Creating _func class
+class _func:
+    pass    # TODO: Stub
+
+
 # Defining new_dialog function
 def new_dialog():
-    pass    # TODO: Stub
+
+    # Setting up the toplevel window
+    window = ctk.CTkToplevel()
+    window.title("0x1de-NX | New Dialog")
+    window.resizable(False, False)
+    window.geometry("300x200")
+
+    # Setting focus on the window
+    window.focus_set()
+    window.grab_set()
+
+    # Creating variables
+    object_types_list = [
+        "File",
+        "Directory",
+    ]
+
+    # Creating the frames
+    details_frame = ctk.CTkFrame(   # Frame for object details stuff
+        master=window,
+        height=275,
+        fg_color="#242424",
+    )
+    details_frame.pack(side="top", fill="both")
+
+    # Creating the new window's children
+    close_button = ctk.CTkButton(   # Close Button
+        master=window,
+        text="Close",
+    )
+    close_button.pack(side="left", anchor="s")
+
+    create_button = ctk.CTkButton(  # Create Button
+        master=window,
+        text="Create",
+    )
+    create_button.pack(side="right", anchor="s")
+
+    new_object_type_label = ctk.CTkLabel(
+        master=details_frame,
+        text="New Object Type",
+        anchor="w",
+    )
+    new_object_type_label.grid(row=0, column=0, padx=20, pady=10)
+
+    new_object_type_OptionMenu = ctk.CTkOptionMenu(  # Object type OptionMenu
+        master=details_frame,
+        values=object_types_list,
+    )
+    new_object_type_OptionMenu.grid(row=0, column=1, padx=20, pady=10)
+
+    object_name_label = ctk.CTkLabel(   # Object name Label
+        master=details_frame,
+        text="Object Name",
+        anchor="w",
+    )
+    object_name_label.grid(row=1, column=0, padx=20, pady=10)
+
+    object_name_entry = ctk.CTkEntry(   # Object name Entry
+        master=details_frame,
+        placeholder_text="Enter Name Here!",
+    )
+    object_name_entry.grid(row=1, column=1, padx=20, pady=10)
+
+
