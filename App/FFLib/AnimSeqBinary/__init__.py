616f3e458a1ee4554b2293e758b630949f7a8190
VillainousSsnake <cjdiamond9@gmail.com>
2024-04-15 12:38:12 -0400

    Added some code and an asb_dt import statement

--- App/FFLib/AnimSeqBinary/__init__.py
+++ App/FFLib/AnimSeqBinary/__init__.py
@@ -2,10 +2,13 @@
 # Contains ASB class
 
 # Importing modules, libraries, and packages
+from App.FFLib.AnimSeqBinary import asb_dt
 import os
 
 
 # ASB Class
 class ASB:
-    def __init__(self):
-        pass    # TODO: Stub
+    def __init__(self, file_path):
+
+        # Creating file path variable
+        self.file_path = file_path
