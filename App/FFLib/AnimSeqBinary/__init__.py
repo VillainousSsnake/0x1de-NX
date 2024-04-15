6d4206e280259e631106cac64d24a46400d5b64a
VillainousSsnake <cjdiamond9@gmail.com>
2024-04-15 12:44:31 -0400

    Finished __init__ method, added to_json and to_asb stub methods

--- App/FFLib/AnimSeqBinary/__init__.py
+++ App/FFLib/AnimSeqBinary/__init__.py
@@ -2,13 +2,39 @@
 # Contains ASB class
 
 # Importing modules, libraries, and packages
-from App.FFLib.AnimSeqBinary import asb_dt
+from App.FFLib.AnimSeqBinary.asb_dt import asb
 import os
 
 
 # ASB Class
 class ASB:
-    def __init__(self, file_path):
+    def __init__(self, file_path: os.PathLike | str):
+        """
+        ASB class to convert ASB files to json and back.
+        :param file_path: input to the ASB file
+        """
 
         # Creating file path variable
         self.file_path = file_path
+
+        # Getting file data
+        with open(self.file_path, "rb") as f_in:
+            binary_data = f_in.read()
+
+        # Creating asb controller
+        self.asb_controller = asb.ASB(binary_data)
+
+    def to_json(self) -> str:
+        """
+        Converts ASB to JSON.
+        :return: JSON string
+        """
+        pass    # TODO: Stub
+
+    def to_asb(self, data) -> bytes:
+        """
+        Converts JSON string to ASB data.
+        :param data: JSON string
+        :return: ASB data (in bytes)
+        """
+        pass    # TODO: Stub
