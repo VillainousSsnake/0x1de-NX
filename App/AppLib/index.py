be34c57a499fedd3c2f84eeb1b47268ee378123b
VillainousSsnake <cjdiamond9@gmail.com>
2024-04-17 16:28:20 -0400

    Added code to launch_plugin_menu_node

--- App/AppLib/index.py
+++ App/AppLib/index.py
@@ -9,6 +9,11 @@ from App.GUI.main_menu import main_menu as main_menu_redirect
 # Importing local modules
 from App.AppLib.plugin_handler import PluginHandler
 
+# Importing packages
+from functools import partial
+import importlib.util
+import os
+
 
 # Index class (Holds menu functions)
 class Index:
@@ -24,6 +29,37 @@ class Index:
         project_editor_redirect(app)
 
     @staticmethod
-    def launch_plugin_menu_node(app, node_name):
-        enabled_plugins_data = PluginHandler.get_enabled_plugins()  # TODO: Finish
+    def launch_plugin_menu_node(app, current_menu_node, plugin_dir):
+
+        # Getting all the variables
+        command_name = current_menu_node["CommandName"]
+        command_file = current_menu_node["CommandFile"]
+        raw_params = current_menu_node["CommandParams"]
+
+        # Getting the command
+        file_path = os.path.join(
+            plugin_dir, command_file
+        )
+        spec = importlib.util.spec_from_file_location(command_name, file_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+
+        # Creating command
+        command = getattr(module, command_name)
+
+        # Configuring command params
+        command_params_dict = {}
+        raw_params = current_menu_node["CommandParams"]
+
+        for item in raw_params:
+            if item == 'App':
+                command_params_dict["App"] = app
+            elif item == 'Root':
+                command_params_dict["Root"] = root
+
+        command = partial(command, command_params_dict)
+
+        # Running command
+        command()
+        print("Done!")
 
