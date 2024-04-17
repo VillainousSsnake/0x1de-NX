4f0e37b44fb6ccef4f67205e7fb48f3ea6439950
VillainousSsnake <cjdiamond9@gmail.com>
2024-04-17 11:16:37 -0400

    Added import statement and removed settings_menu index

--- App/AppLib/index.py
+++ App/AppLib/index.py
@@ -3,8 +3,11 @@
 
 # Importing GUI
 from App.GUI.project_editor import project_editor as project_editor_redirect
-from App.GUI.main_menu import main_menu as main_menu_redirect
 from App.GUI.SubWin.project_editor.settings_menu import settings_menu as settings_menu_redirect
+from App.GUI.main_menu import main_menu as main_menu_redirect
+
+# Importing local modules
+from App.AppLib.plugin_handler import PluginHandler
 
 
 # Index class (Holds menu functions)
@@ -19,8 +22,4 @@ class Index:
     def project_editor(app):
 
         project_editor_redirect(app)
-
-    @staticmethod
-    def settings_menu(app):
-        settings_menu_redirect(app)
 
