f21fe1db500eaa811dbd93d2391beacff5f67e51
VillainousSsnake <cjdiamond9@gmail.com>
2024-04-17 11:39:52 -0400

    Added code to the no cases match but plugins enabled plugins case

--- 0x1de-NX.py
+++ 0x1de-NX.py
@@ -177,7 +177,14 @@ while app.returnStatement != "exit":
                 )
 
             else:   # Otherwise, looking for enabled plugins with menu nodes
-                pass    # TODO: Stub
+
+                enabled_plugins = PluginHandler.get_enabled_plugins()
+
+                menu_node_plugins = {}
+
+                for key in enabled_plugins:
+                    if PluginHandler.get_menu_node_from_json(enabled_plugins[key]) is not None:
+                        pass    # TODO: Stub
 
 # Clearing the temp folder
 temp_folder = os.path.join(os.getenv("LOCALAPPDATA"), "0x1de-NX", "_temp_")
