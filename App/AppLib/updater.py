ff3be4ba835414fe0020bb46ceb5958c9b23c00f
VillainousSsnake <cjdiamond9@gmail.com>
2024-05-07 16:57:02 -0400

    Added docstring and function return type definitions

--- App/AppLib/updater.py
+++ App/AppLib/updater.py
@@ -17,12 +17,14 @@ CurrentVersionGlobal = {
 # Updater class
 class Updater:
     @staticmethod
-    def get_app_version():
+    def get_app_version() -> dict:
+        """Returns the current app version"""
 
         return CurrentVersionGlobal
 
     @staticmethod
-    def get_latest_version():
+    def get_latest_version() -> dict:
+        """Fetches the latest_version.json data from the 0x1de-NX repository"""
 
         url = f"https://raw.githubusercontent.com/VillainousSsnake/0x1de-NX/main/latest_version.json"
         response = requests.get(url)
@@ -30,7 +32,8 @@ class Updater:
         return data["latest_app_version"]
 
     @staticmethod
-    def check_for_updates():
+    def check_for_updates() -> None:
+        """Creates messagebox.showinfo object to notify the user of outdated client or up to date client"""
 
         # Creating the version output strings
         current_ver_out = str(CurrentVersionGlobal['type'] + " " + CurrentVersionGlobal['version'])
@@ -48,9 +51,7 @@ class Updater:
 
     @staticmethod
     def is_outdated_client() -> bool:
-        """
-        Returns bool for if the client is outdated or not.
-        """
+        """Returns bool for if the client is outdated or not."""
 
         # Returning True if out of date
         if CurrentVersionGlobal["version"] != Updater.get_latest_version()["version"]:
