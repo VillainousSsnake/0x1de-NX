825be5fb6b7c9c35ad0a7454a2a6d68ff32e0b53
VillainousSsnake <cjdiamond9@gmail.com>
2024-05-12 19:25:42 -0400

    Added setup_menu.py (Stub)

---
+++ App/GUI/setup_menu.py
@@ -0,0 +1,18 @@
+# /App/GUI/setup_menu.py
+
+# Import statements
+import customtkinter as ctk
+
+
+# ButtonFunc class (Adapted from AINB-Toolbox)
+class ButtonFunc:
+    pass    # TODO: Stub
+
+
+class _func:
+    pass    # TODO: Stub
+
+
+# Settings menu function
+def settings_menu(root, app):
+    pass    # TODO: Stub
