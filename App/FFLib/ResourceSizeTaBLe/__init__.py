7289422c99eb32e5345ffcdadf9a639eb6947eda
VillainousSsnake <cjdiamond9@gmail.com>
2024-06-18 18:19:34 -0400

    Added stub RESTBL package

---
+++ App/FFLib/ResourceSizeTaBLe/__init__.py
@@ -0,0 +1,19 @@
+# App/FFLib/StandardArchive/__init__.py
+# Contains Sarc class
+
+# Importing dependencies
+import os
+import subprocess
+
+
+# RESTBL class
+class RESTBL:
+
+    @staticmethod
+    def generate_restbl_from_folder(folder_path, romfs_path):
+        """
+        Generates a ResourceSizeTaBLe file for the given project path.
+        :param folder_path: The input path for the folder that the function generates a rstb file from.
+        :param romfs_path: The path to the romfs dump folder.
+        """
+        pass    # TODO: Stub
