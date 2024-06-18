4b5c9e2ce6352cb00b085e3a454cccbfaaa6c99f
VillainousSsnake <cjdiamond9@gmail.com>
2024-06-18 18:32:45 -0400

    Added experimental code to generate_restbl_from_folder static method in RESTBL class

--- App/FFLib/ResourceSizeTaBLe/__init__.py
+++ App/FFLib/ResourceSizeTaBLe/__init__.py
@@ -10,10 +10,28 @@ import subprocess
 class RESTBL:
 
     @staticmethod
-    def generate_restbl_from_folder(folder_path, romfs_path):
+    def generate_restbl_from_folder(folder_path: os.PathLike | str):
         """
         Generates a ResourceSizeTaBLe file for the given project path.
         :param folder_path: The input path for the folder that the function generates a rstb file from.
-        :param romfs_path: The path to the romfs dump folder.
         """
+
+        # Getting the path to restbl tool
+        rstb_tool_path = os.path.join(os.getcwd(), "App", "Bin", "rstb_tool", "restbl.exe")
+
+        # Getting the base command
+        base_command = str(
+            rstb_tool_path + ' --action single-mod --use-checksums --compress --mod-path "'
+            + folder_path + '" --version '
+        )
+
+        # Running command with all game versions
+        for version in ["100", "110", "111", "120", "121"]:
+            command = base_command + version
+            subprocess.run(command)
+            print(f"restbl {version} generated")
+
+        # Output
+        print("Done!")
+
         pass    # TODO: Stub
