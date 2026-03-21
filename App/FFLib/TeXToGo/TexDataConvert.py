1ba73029899d035e48723a5841139f1b8c0b9fbb
VillainousSsnake <cjdiamond9@gmail.com>
2026-03-21 19:19:09 -0400

    Added full bc4 support, added broken bc5 support

--- App/FFLib/TeXToGo/TexDataConvert.py
+++ App/FFLib/TeXToGo/TexDataConvert.py
@@ -2,10 +2,62 @@
 # Importing dependencies
 from App.FFLib.TeXToGo import TexToGo_base
 from App.FFLib.TotkZsDic import ZsDic
+from tkinter import messagebox
 import texture2ddecoder
 import py_tegra_swizzle
 from PIL import Image
 import math
+
+
+# Converter class
+class Converter:
+    """
+    This class contains functions to convert
+    raw texture data to popular image formats
+    like png, jpg, etc.
+    """
+
+    @staticmethod
+    def to_png(controller, data, out_path):
+        """
+        takes image format and read-binary io stream and returns png data
+        """
+        print("Detecting texture format...")
+        match str(controller.Format):
+
+            case "TEX_FORMAT.BC1_UNORM":   # BC1 decoding
+                print("BC1_UNORM detected!")
+                bc1_to_png(controller, data, out_path)
+
+            case "TEX_FORMAT.BC4_UNORM":  # BC4 decoding
+                print("BC4_UNORM detected!")
+                bc4_to_png(controller, data, out_path)
+
+            case "TEX_FORMAT.BC5_UNORM":    # BC5 decoding
+                print("BC5_UNORM detected!")
+
+                # Not fully implemented message
+                # TODO: Finish bc5 support then remove this
+                if not messagebox.askyesno(
+                        "Not Implemented Message",
+                        "BC5_UNORM Format is not fully implemented, proceed?"):
+                    return None
+
+                bc5_to_png(controller, data, out_path)
+
+            case _:     # Throwing type error
+                TypeError("Image isn't a valid texture format")
+
+    @staticmethod
+    def to_txtg(controller, file_in, out_path):
+        pass    # TODO: Stub
+
+
+"""
+            |--------------------------------|
+            | Functions for converter class: |
+            |--------------------------------|
+"""
 
 
 def get_block_height(height):
@@ -49,29 +101,71 @@ def bc1_to_png(controller: TexToGo_base.TXTG, data, out_path):
     print("saved!")
 
 
-# Converter class
-class Converter:
-    """
-    This class contains functions to convert
-    raw texture data to popular image formats
-    like png, jpg, etc.
-    """
+def bc4_to_png(controller: TexToGo_base.TXTG, data, out_path):
 
-    @staticmethod
-    def to_png(controller, data, out_path):
-        """
-        takes image format and read-binary io stream and returns png data
-        """
-        print("Detecting texture format...")
-        match str(controller.Format):
+    # Decompressing zstandard data
+    data = ZsDic.auto_decompress_bytes(data)
 
-            case "TEX_FORMAT.BC1_UNORM":   # BC1 decoding
-                print("bc1_unorm detected!")
-                bc1_to_png(controller, data, out_path=out_path)
+    block_width = max(1, controller.Width // 4)
+    block_height_dim = max(1, controller.Height // 4)
 
-            case _:     # Throwing type error
-                TypeError("Image isn't a valid texture format")
+    block_h = get_block_height(block_height_dim)
 
-    @staticmethod
-    def to_txtg(controller, file_in, out_path):
-        pass    # TODO: Stub
+    print(f"Data length: {len(data)}, Expected: {controller.Width * controller.Height // 2}")
+    print(f"Block dims: {block_width}x{block_height_dim}, GOB block_height: {block_h}")
+
+    deswizzled_bytes = py_tegra_swizzle.deswizzle_block_linear(
+        width=block_width,
+        height=block_height_dim,
+        depth=controller.HeaderInfo.Depth,
+        source=data,
+        block_height=block_h,
+        bytes_per_pixel=8,
+    )
+
+    # Decode BC1 to raw RGBA bytes
+    rgba_bytes = texture2ddecoder.decode_bc4(deswizzled_bytes, controller.Width, controller.Height)
+
+    # Create an image from RGBA bytes
+    img = Image.frombytes("RGBA", (controller.Width, controller.Height), rgba_bytes)
+
+    # Save as PNG
+    img.save(out_path, format="PNG")
+    print(f"Decoded texture saved to {out_path}")
+    print("saved!")
+
+
+def bc5_to_png(controller: TexToGo_base.TXTG, data, out_path):  # TODO: Fix this later
+
+    # Decompressing zstandard data
+    data = ZsDic.auto_decompress_bytes(data)
+
+    block_width = max(1, controller.Width // 4)
+    block_height_dim = max(1, controller.Height // 4)
+
+    block_h = get_block_height(block_height_dim)
+
+    print(f"Data length: {len(data)}, Expected: {controller.Width * controller.Height // 2}")
+    print(f"Block dims: {block_width}x{block_height_dim}, GOB block_height: {block_h}")
+
+    deswizzled_bytes = py_tegra_swizzle.deswizzle_block_linear(
+        width=block_width,
+        height=block_height_dim,
+        depth=controller.HeaderInfo.Depth,
+        source=data,
+        block_height=block_h,
+        bytes_per_pixel=8,
+    )
+
+    # Decode BC1 to raw RGBA bytes
+    rgba_bytes = texture2ddecoder.decode_bc5(deswizzled_bytes, controller.Width, controller.Height)
+
+    # Create an image from RGBA bytes
+    img = Image.frombytes("RGBA", (controller.Width, controller.Height), rgba_bytes)
+
+    # Save as PNG
+    img.save(out_path, format="PNG")
+    print(f"Decoded texture saved to {out_path}")
+    print("saved!")
+
+
