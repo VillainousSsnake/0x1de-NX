c2230a02f45de444cbf27df14e13031cf7d85fd8
VillainousSsnake <cjdiamond9@gmail.com>
2026-03-31 17:15:16 -0400

    added rename file functionality

--- App/GUI/SubWin/project_editor/refactor_menu.py
+++ App/GUI/SubWin/project_editor/refactor_menu.py
@@ -1,5 +1,6 @@
 # /App/GUI/SubWin/project_editor/refactor_menu.py
 # Contains code for refactor dialog
+import tkinter
 
 # TODO: Convert code past here to rename a file in the project tree instead of renaming a project
 
@@ -8,6 +9,7 @@ import App.AppLib.customtkinter as ctk
 from tkinter import messagebox
 from functools import partial
 import json
+import pathlib
 import os
 
 
@@ -18,7 +20,7 @@ class ButtonFunc:
         window.destroy()
 
     @staticmethod
-    def rename(window, app, root, project_name_entry: ctk.CTkEntry):
+    def rename():
         pass    # TODO: Stub
 
 
@@ -27,7 +29,7 @@ def refactor_menu(cur_item, project_treeview, app):
 
     # Setting up the toplevel window
     window = ctk.CTkToplevel()
-    window.title("0x1de-NX | Refactor")
+    window.title("0x1de-NX | Rename Object")
     window.resizable(False, False)
     window.geometry("600x300")
 
@@ -35,10 +37,8 @@ def refactor_menu(cur_item, project_treeview, app):
     window.focus_set()
     window.grab_set()
 
-    print("Refactoring!")  # TODO: Remove
-    print(cur_item)
+    print(cur_item)  # TODO: Remove
 
-    object_name = os.path.basename(cur_item['values'][0])
     object_type = cur_item['tags'][0]
 
     match object_type:
@@ -51,7 +51,45 @@ def refactor_menu(cur_item, project_treeview, app):
         # In the case where the object is a file
         case "File":
 
-            pass    # TODO: Stub
+            filename = os.path.basename(cur_item['values'][0])
+            filetype = pathlib.Path(cur_item['values'][0]).suffixes
+
+            # Creating and packing Info frame
+            frame = ctk.CTkFrame(
+                master=window,
+            )
+            frame.pack(anchor="nw", fill=ctk.BOTH)
+
+            # Configuring the menu widgets
+            file_name_label = ctk.CTkLabel(
+                master=frame,
+                text=str("Rename File '" + filename + "' to:"),
+                padx=10, pady=10,
+            )
+            file_name_label.pack(side="top", anchor='w')
+
+            file_name_entry = ctk.CTkEntry(
+                master=frame,
+                placeholder_text=filename,
+            )
+            file_name_entry.pack(side="top", fill='x')
+            file_name_entry.insert(0, filename)
+
+            file_name_entry.bind()
+
+            rename_button = ctk.CTkButton(
+                master=window,
+                text="Rename",
+                command=partial(ButtonFunc.rename)
+            )
+            rename_button.pack(anchor="se", side="right")
+
+            cancel_button = ctk.CTkButton(
+                master=window,
+                text="Cancel",
+                command=partial(ButtonFunc.cancel, window),
+            )
+            cancel_button.pack(anchor="se", side="left")
 
         case _:     # Throwing type error
             TypeError("Object is not a Directory or File")
